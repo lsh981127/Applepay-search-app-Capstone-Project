4c519d0f1aa7781022f44c60607201da9fa272ea
tnfus916 <tnfus1999@gmail.com>
2023-05-21 11:13:16 +0900

    중부원점좌표계 식 수정

--- lib/src/googleMap.dart
+++ lib/src/googleMap.dart
@@ -85,7 +85,7 @@ class _googleMapPageState extends State<googleMapPage> {
     final valueRange = await worksheet?.values.allRows();
     final data=valueRange?.map((row) => List<dynamic>.from(row)).toList();
 
-    var srcProj=Projection.add('EPSG:2097','+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43');
+    var srcProj=Projection.add('EPSG:2097','+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43');
     var dstnProj=Projection.get('EPSG:4326')!;
 
     List<List>? rowsAsListOfValues = data;
