f2db9b0fc630fec232c92c31656e06459e74a5a4
jki1017 <jki101774@gmail.com>
2023-06-06 20:13:08 +0900

    Update googleMap.dart

--- lib/src/googleMap.dart
+++ lib/src/googleMap.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:csv/csv.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
@@ -621,6 +620,7 @@ class _googleMapPageState extends State<googleMapPage> {
             ),
           ),
         ),
+        Visibility(child: button),
         SizedBox(
           height: 50,
           child: ListView.builder(
