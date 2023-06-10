463b9645fb1b10db6a78a02dab8aa71db66e2e37
tnfus916 <tnfus1999@gmail.com>
2023-06-11 00:25:44 +0900

    게시글 작성 구현 완
    글쓴이, 날짜시간, 공감수, 댓글수 내용 입력은 추후에 구현 예정

--- lib/community/write_item.dart
+++ lib/community/write_item.dart
@@ -4,12 +4,33 @@ import 'package:newcapstone/community/components/custom_elevated_button.dart';
 import 'package:newcapstone/community/components/custom_textarea.dart';
 import 'package:newcapstone/community/components/custom_text_form_field.dart';
 import 'package:newcapstone/community/freeforum.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-import 'components/custom_text_form_field.dart';
+class WritePage extends StatefulWidget{
+  @override
+  _WritePageState createState()=>_WritePageState();
+}
 
-class WritePage extends StatelessWidget {
-  
+class _WritePageState extends State<WritePage>{
   final _formKey = GlobalKey<FormState>();
+  final firestore=FirebaseFirestore.instance;
+
+  final _titleTextEditController=TextEditingController();
+  final _contentTextEditController=TextEditingController();
+
+  @override
+  void dispose(){
+    _titleTextEditController.dispose();
+    _contentTextEditController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context){
@@ -22,20 +43,52 @@ class WritePage extends StatelessWidget {
         child: Form(
           key: _formKey,
           child: ListView(
-                children: [
-                  CustomTextFormField(hint: "Title", funValidator: (value) {}),
-                  const CustomTextArea(hint: "Content"),
-                  CustomElevatedButton(
-                      text: "글쓰기 완료",
-                      funPageRoute: () {
-                        if(_formKey.currentState!.validate()){
-                          Get.off(freeForum());
-                        }
-                      }
-                  )],
+            children: [
+              CustomTextFormField(
+                  hint: "Title",
+                  controller: _titleTextEditController,
               ),
-            ),
+              CustomTextArea(
+                  hint: "Content",
+                  controller:_contentTextEditController,
+              ),
+              CustomElevatedButton(
+                  text: "글쓰기 완료",
+                  getData:(){
+                    final String title=_titleTextEditController.text;
+                    final content=_contentTextEditController.text;
+                    // user 정보 가져오기
+                    // final user=
+                    final userCollectionReference=
+                      FirebaseFirestore.instance.collection("users").doc('${FirebaseAuth.instance.currentUser?.uid}').collection('myposts').doc(title);
+                    userCollectionReference.set({
+                      "title":title,
+                      "content":content,
+                      // "user": ,
+                      //날짜+시간: ,
+                      //공감수: ,
+                      //댓글수: ,
+                    });
+                    final postCollectionReference=
+                    FirebaseFirestore.instance.collection("posts").doc(title);
+                    postCollectionReference.set({
+                      "title":title,
+                      "content":content,
+                      // "user": ,
+                      //날짜+시간: ,
+                      //공감수: ,
+                      //댓글수: ,
+                    });
+                  },
+                  funPageRoute: () {
+                    if(_formKey.currentState!.validate()){
+                      Get.off(freeForum());
+                    }
+                  }
+              )],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
